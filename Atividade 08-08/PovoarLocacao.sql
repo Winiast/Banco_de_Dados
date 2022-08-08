e278407898009ca0ab0ba895bf9321324148b22f
Gustavo Ferreira <82901156+Winiast@users.noreply.github.com>
2022-08-08 11:29:52 -0300

    Povoamento do banco

---
+++ Atividade 08-08/PovoarLocacao.sql
@@ -0,0 +1,61 @@
+INSERT INTO CLIENTE (nome,endereco) VALUES ("Gustavo", "Rua do Zezinhho 925");
+INSERT INTO CLIENTE (nome,endereco) VALUES ("Pedrinho", "Rua do Dirceu 3022");
+INSERT INTO CLIENTE (nome,endereco) VALUES ("Andre", "Rua do Campetes 0009");
+INSERT INTO CLIENTE (nome,endereco) VALUES ("Bartolome", "Rua do São Sebastiao 4423");
+INSERT INTO CLIENTE (nome,endereco) VALUES ("Moacir", "Rua do Tigre 3635");
+
+INSERT INTO PESSOA_JURIDICIA (cnpj, inscricao_estadual) VALUES ()
+INSERT INTO PESSOA_JURIDICIA (cnpj, inscricao_estadual) VALUES ()
+INSERT INTO PESSOA_JURIDICIA (cnpj, inscricao_estadual) VALUES ()
+INSERT INTO PESSOA_JURIDICIA (cnpj, inscricao_estadual) VALUES ()
+INSERT INTO PESSOA_JURIDICIA (cnpj, inscricao_estadual) VALUES ()
+
+
+INSERT INTO PESSOA_FISICA (cpf, sexo, data_nascimento) VALUES ()
+INSERT INTO PESSOA_FISICA (cpf, sexo, data_nascimento) VALUES ()
+INSERT INTO PESSOA_FISICA (cpf, sexo, data_nascimento) VALUES ()
+INSERT INTO PESSOA_FISICA (cpf, sexo, data_nascimento) VALUES ()
+INSERT INTO PESSOA_FISICA (cpf, sexo, data_nascimento) VALUES ()
+
+INSERT INTO MARCA (descricao) VALUES ()
+INSERT INTO MARCA (descricao) VALUES ()
+INSERT INTO MARCA (descricao) VALUES ()
+INSERT INTO MARCA (descricao) VALUES ()
+INSERT INTO MARCA (descricao) VALUES ()
+
+
+INSERT INTO CODIGO_RESERVA (codigo_tipo_veiculo, codigo_cliente, data_reserva, data_retirada_veiculo) VALUES ()
+INSERT INTO CODIGO_RESERVA (codigo_tipo_veiculo, codigo_cliente, data_reserva, data_retirada_veiculo) VALUES ()
+INSERT INTO CODIGO_RESERVA (codigo_tipo_veiculo, codigo_cliente, data_reserva, data_retirada_veiculo) VALUES ()
+INSERT INTO CODIGO_RESERVA (codigo_tipo_veiculo, codigo_cliente, data_reserva, data_retirada_veiculo) VALUES ()
+INSERT INTO CODIGO_RESERVA (codigo_tipo_veiculo, codigo_cliente, data_reserva, data_retirada_veiculo) VALUES ()
+
+INSERT INTO MODELO (codigo_marca, descricao) VALUES ()
+INSERT INTO MODELO (codigo_marca, descricao) VALUES ()
+INSERT INTO MODELO (codigo_marca, descricao) VALUES ()
+INSERT INTO MODELO (codigo_marca, descricao) VALUES ()
+INSERT INTO MODELO (codigo_marca, descricao) VALUES ()
+
+INSERT INTO TIPO_VEICULO (descricao, porte, tamanho, numero_passageiros, numero_portas, ar_condicionado, multimidia, direcao_hidraulica, cambio_automatico, capacidade_carga) VALUES ()
+INSERT INTO TIPO_VEICULO (descricao, porte, tamanho, numero_passageiros, numero_portas, ar_condicionado, multimidia, direcao_hidraulica, cambio_automatico, capacidade_carga) VALUES ()
+INSERT INTO TIPO_VEICULO (descricao, porte, tamanho, numero_passageiros, numero_portas, ar_condicionado, multimidia, direcao_hidraulica, cambio_automatico, capacidade_carga) VALUES ()
+INSERT INTO TIPO_VEICULO (descricao, porte, tamanho, numero_passageiros, numero_portas, ar_condicionado, multimidia, direcao_hidraulica, cambio_automatico, capacidade_carga) VALUES ()
+INSERT INTO TIPO_VEICULO (descricao, porte, tamanho, numero_passageiros, numero_portas, ar_condicionado, multimidia, direcao_hidraulica, cambio_automatico, capacidade_carga) VALUES ()
+
+INSERT INTO RESERVA (codigo_tipo_veiculo, codigo_cliente, data_reserva, data_retirada_veiculo) VALUES ()
+INSERT INTO RESERVA (codigo_tipo_veiculo, codigo_cliente, data_reserva, data_retirada_veiculo) VALUES ()
+INSERT INTO RESERVA (codigo_tipo_veiculo, codigo_cliente, data_reserva, data_retirada_veiculo) VALUES ()
+INSERT INTO RESERVA (codigo_tipo_veiculo, codigo_cliente, data_reserva, data_retirada_veiculo) VALUES ()
+INSERT INTO RESERVA (codigo_tipo_veiculo, codigo_cliente, data_reserva, data_retirada_veiculo) VALUES ()
+
+INSERT INTO VEICULO (codigo_modelo, codigo_tipo_veiculo,cnpj_locadora, chassi, numero_motor, status_veiculo, km_atual) VALUES ()
+INSERT INTO VEICULO (codigo_modelo, codigo_tipo_veiculo,cnpj_locadora, chassi, numero_motor, status_veiculo, km_atual) VALUES ()
+INSERT INTO VEICULO (codigo_modelo, codigo_tipo_veiculo,cnpj_locadora, chassi, numero_motor, status_veiculo, km_atual) VALUES ()
+INSERT INTO VEICULO (codigo_modelo, codigo_tipo_veiculo,cnpj_locadora, chassi, numero_motor, status_veiculo, km_atual) VALUES ()
+INSERT INTO VEICULO (codigo_modelo, codigo_tipo_veiculo,cnpj_locadora, chassi, numero_motor, status_veiculo, km_atual) VALUES ()
+
+INSERT ALUGUEL (placa, cnpj_locadora_origem, cnpj_locadora_destino, codigo_cliente, data_ ,data_prevista_entrega, data_entrega,nome_motorista,cnh_motorista, data_validade, km_rodada, valor) VALUES ()
+INSERT ALUGUEL (placa, cnpj_locadora_origem, cnpj_locadora_destino, codigo_cliente, data_ ,data_prevista_entrega, data_entrega,nome_motorista,cnh_motorista, data_validade, km_rodada, valor) VALUES ()
+INSERT ALUGUEL (placa, cnpj_locadora_origem, cnpj_locadora_destino, codigo_cliente, data_ ,data_prevista_entrega, data_entrega,nome_motorista,cnh_motorista, data_validade, km_rodada, valor) VALUES ()
+INSERT ALUGUEL (placa, cnpj_locadora_origem, cnpj_locadora_destino, codigo_cliente, data_ ,data_prevista_entrega, data_entrega,nome_motorista,cnh_motorista, data_validade, km_rodada, valor) VALUES ()
+INSERT ALUGUEL (placa, cnpj_locadora_origem, cnpj_locadora_destino, codigo_cliente, data_ ,data_prevista_entrega, data_entrega,nome_motorista,cnh_motorista, data_validade, km_rodada, valor) VALUES ()
