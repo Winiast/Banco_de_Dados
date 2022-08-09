2e8f440d782cf77df1e24c6cc4de229d111736d7
André Luiz V <89608887+KyuujiVinsmoke@users.noreply.github.com>
2022-08-09 01:59:41 -0300

    Atividade Finalizada

--- Atividade 08-08/locadora_de_veiculos.sql
+++ Atividade 08-08/locadora_de_veiculos.sql
@@ -1,114 +1,126 @@
-create table locadora_veiculos_locadora(
-	cnpj varchar(20) not null,
-	razao_social varchar(100),
-	endereco varchar(255),
-	primary key(cnpj)
+create table locadora_veiculos_locadora
+(
+	cnpj_locadora varchar (20) not null,
+	razao_social_locadora varchar (100),
+	endereco_locadora varchar (255),
+	primary key (cnpj_locadora)
 );
 
-create table locadora_veiculos_locadora_telefone(
-	telefone varchar(20) not null,
-	cnpj varchar(20) not null,
-	primary key(telefone, cnpj),
-	foreign key(cnpj) REFERENCES locadora_veiculos_locadora(cnpj) 
+create table locadora_veiculos_telefone
+(
+	telefone_telefone varchar (20) not null,
+	cnpj_locadora varchar (20) not null,
+	primary key (telefone_telefone),
+	foreign key (cnpj_locadora) REFERENCES locadora_veiculos_locadora (cnpj_locadora) 
 );
 
-create table locadora_veiculos_tipo_veiculo(
-	codigo int not null AUTO_INCREMENT,
-	descricao varchar(255) not null,
-	porte varchar(45) not null,
-	tamanho varchar(45) null,
-	numero_passageiros int null,
-	numero_portas int null,
-	arcondicionado Bit null,
-	multimidia Bit null,
-	direcao_hidraulica Bit null,
-	cambio_automatico Bit null,
-	capacidade_carga Bit null,
-	primary key(codigo)
+create table locadora_veiculos_tipo_veiculo
+(
+	codigo_tipo_veiculo int not null AUTO_INCREMENT,
+	descricao_tipo_veiculo varchar (255) not null,
+	porte_tipo_veiculo varchar (45) not null,
+	tamanho_tipo_veiculo varchar (45) null,
+	numero_passageiros_tipo_veiculo int null,
+	numero_portas_tipo_veiculo int null,
+	arcondicionado_tipo_veiculo Bit null,
+	multimidia_tipo_veiculo Bit null,
+	direcao_hidraulica_tipo_veiculo Bit null,
+	cambio_automatico_tipo_veiculo Bit null,
+	capacidade_carga_tipo_veiculo Bit null,
+	primary key (codigo_tipo_veiculo)
 );
 
-create table locadora_veiculos_marca(
-	codigo int not null AUTO_INCREMENT,
-	descricao varchar(100),
-	primary key(codigo)
+create table locadora_veiculos_marca
+(
+	codigo_marca int not null AUTO_INCREMENT,
+	descricao_marca varchar (100),
+	primary key (codigo_marca)
 );
 
-create table locadora_veiculos_modelo(
-	codigo int not null AUTO_INCREMENT,
+create table locadora_veiculos_modelo
+(
+	codigo_modelo int not null AUTO_INCREMENT,
+	descricao_modelo varchar (255) not null,
 	codigo_marca int not null,
-	descricao varchar(255) not null,
-	primary key(codigo),
-	foreign key(codigo_marca) REFERENCES locadora_veiculos_marca(codigo)
+	primary key (codigo_modelo),
+	foreign key (codigo_marca) REFERENCES locadora_veiculos_marca (codigo_marca)
 );
 
-create table locadora_veiculos_veiculo(
-	placa varchar(7) not null,
+create table locadora_veiculos_veiculo
+(
+	placa_veiculo varchar (7) not null,
+	chassi_veiculo varchar (45) not null,
+	numero_motor_veiculo varchar (45) not null,
+	status_veiculo varchar (45) not null,
+	km_atual_veiculo varchar (45) not null,
 	codigo_modelo int not null,
-	codigo_tipo_Veiculo int not null,
-	cnpj_locadora varchar(20) not null,
-	chassi varchar(45) not null,
-	numero_motor varchar(45) not null,
-	status_veiculo varchar(45) not null,
-	km_atual varchar(45) not null,
-	primary key(placa),
-	foreign key(codigo_modelo) REFERENCES locadora_veiculos_modelo(codigo),
-    foreign key (codigo_tipo_veiculo) REFERENCES locadora_veiculos_tipo_veiculo(codigo),
-    foreign key(cnpj_locadora) REFERENCES locadora_veiculos_locadora(cnpj)
+	codigo_tipo_veiculo int not null,
+	cnpj_locadora varchar (20) not null,
+	primary key (placa_veiculo),
+	foreign key (codigo_modelo) REFERENCES locadora_veiculos_modelo (codigo_modelo),
+    foreign key (codigo_tipo_veiculo) REFERENCES locadora_veiculos_tipo_veiculo (codigo_tipo_veiculo),
+    foreign key (cnpj_locadora) REFERENCES locadora_veiculos_locadora (cnpj_locadora)
 );
 
-create table locadora_veiculos_cliente(
-	codigo int not null AUTO_INCREMENT,
-	nome varchar(255) not null,
-	endereco varchar(255) not null,
-	primary key(codigo)
+create table locadora_veiculos_cliente
+(
+	codigo_cliente int not null AUTO_INCREMENT,
+	nome_cliente varchar (255) not null,
+	endereco_cliente varchar (255) not null,
+	primary key (codigo_cliente)
 );
 
-create table locadora_veiculos_pessoa_fisica(
+create table locadora_veiculos_pessoa_fisica
+(
+	cpf_pessoa_fisica varchar (15) not null,
+	sexo_pessoa_fisica varchar (45) not null,
+	data_nascimento_pessoa_fisica Date not null,
 	codigo_cliente int not null,
-	cpf varchar(15) not null,
-	sexo varchar(45) not null,
-	data_nascimento Date not null,
-	primary key(codigo_cliente),
-	foreign key(codigo_cliente) REFERENCES locadora_veiculos_cliente(codigo)
+	foreign key (codigo_cliente) REFERENCES locadora_veiculos_cliente (codigo_cliente)
 );
 
-create table locadora_veiculos_pessoa_juridica(
+create table locadora_veiculos_pessoa_juridica
+(
+	cnpj_pessoa_juridica varchar (15) not null,
+	inscricao_estadual_pessoa_juridica varchar (20) not null,
 	codigo_cliente int not null,
-	cnpj varchar(15) not null,
-	inscricao_estadual varchar(20) not null,
-	primary key(codigo_cliente),
-	foreign key(codigo_cliente) REFERENCES locadora_veiculos_cliente(codigo)
+	foreign key (codigo_cliente) REFERENCES locadora_veiculos_cliente (codigo_cliente)
 );
 
-create table locadora_veiculos_reserva(
-	codigo int not null AUTO_INCREMENT,
-	codigo_tipo_Veiculo int not null,
-	codigo_cliente int not null,
+create table locadora_veiculos_reserva
+(
+	codigo_reserva int not null AUTO_INCREMENT,
 	data_reserva Date not null,
-	data_retirada_veiculo Date not null,
-	status_reserva varchar(45),
-	primary key(codigo),
-	foreign key(codigo_tipo_Veiculo) REFERENCES locadora_veiculos_tipo_veiculo(codigo),
-	foreign key(codigo_cliente) REFERENCES locadora_veiculos_cliente(codigo)
+	data_retirada_veiculo_reserva Date not null,
+	status_reserva varchar (45),
+	codigo_tipo_veiculo int not null,
+	codigo_cliente int not null,
+	primary key (codigo_reserva),
+	foreign key (codigo_tipo_veiculo) REFERENCES locadora_veiculos_tipo_veiculo (codigo_tipo_veiculo),
+	foreign key (codigo_cliente) REFERENCES locadora_veiculos_cliente (codigo_cliente)
 );
 
-create table locadora_veiculos_aluguel(
-	codigo int not null AUTO_INCREMENT,
-	placa varchar(7) not null,
-	cnpj_locadora_origem varchar(20) not null,
-	cnpj_locadora_destino varchar(20) not null,
-	codigo_cliente int not null,
+create table locadora_veiculos_aluguel
+(
+	codigo_aluguel int not null AUTO_INCREMENT,
 	data_aluguel Date not null,
-	data_prevista_entrega Date not null,
-	data_entrega Date null,
-	nome_motorista varchar(255) not null,
-	cnh_motorista varchar(2),
-	data_validade_cnh Date not null,
-	km_rodada int not null,
-	valor float null,
-	primary key(codigo),
-	foreign key(placa) REFERENCES locadora_veiculos_veiculo(placa),
-	foreign key(cnpj_locadora_origem) REFERENCES locadora_veiculos_locadora(cnpj),
-	foreign key(cnpj_locadora_destino) REFERENCES locadora_veiculos_locadora(cnpj),
-	foreign key(codigo_cliente) REFERENCES locadora_veiculos_cliente(codigo)
+	data_prevista_entrega_aluguel Date not null,
+	data_entrega_aluguel Date null,
+	nome_motorista_aluguel varchar (255) not null,
+	cnh_motorista_aluguel varchar (2),
+	data_validade_cnh_aluguel Date not null,
+	km_rodada_aluguel int not null,
+	valor_aluguel float null,
+	placa_veiculo varchar (7) not null,
+	cnpj_locadora_origem varchar (20) not null,
+	cnpj_locadora_destino varchar (20) not null,
+	codigo_cliente int not null,
+	primary key (codigo_aluguel),
+	foreign key (placa_veiculo) REFERENCES locadora_veiculos_veiculo (placa_veiculo),
+	foreign key (cnpj_locadora_origem) REFERENCES locadora_veiculos_locadora (cnpj_locadora),
+	foreign key (cnpj_locadora_destino) REFERENCES locadora_veiculos_locadora (cnpj_locadora),
+	foreign key (codigo_cliente) REFERENCES locadora_veiculos_cliente (codigo_cliente)
 );
+
+DROP TABLE locadora_veiculos_telefone, locadora_veiculos_veiculo, locadora_veiculos_tipo_veiculo, locadora_veiculos_reserva, locadora_veiculos_pessoa_juridica, 
+locadora_veiculos_pessoa_fisica, locadora_veiculos_modelo, locadora_veiculos_marca, locadora_veiculos_locadora, locadora_veiculos_cliente, locadora_veiculos_aluguel
