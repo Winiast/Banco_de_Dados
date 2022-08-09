eb6f48f71c60d58b3a59f8a08f2fc018c20c6740
André Luiz V <89608887+KyuujiVinsmoke@users.noreply.github.com>
2022-08-09 02:00:25 -0300

    Atividade Finalizada

--- Atividade 08-08/PovoarLocacao.sql
+++ Atividade 08-08/PovoarLocacao.sql
@@ -1,75 +1,85 @@
-INSERT INTO locadora_veiculos_locadora (cnpj, razao_social, endereco) VALUES ("312457", "94336", "sei la");
-INSERT INTO locadora_veiculos_locadora (cnpj, razao_social, endereco) VALUES ("3126757", "947336", "Disney");
-INSERT INTO locadora_veiculos_locadora (cnpj, razao_social, endereco) VALUES ("37657457", "947336", "Orlando");
-INSERT INTO locadora_veiculos_locadora (cnpj, razao_social, endereco) VALUES ("23512457", "944336", "Acre");
-INSERT INTO locadora_veiculos_locadora (cnpj, razao_social, endereco) VALUES ("31562457", "9434536", "Beverly hills");
-//
+INSERT INTO locadora_veiculos_locadora (cnpj_locadora, razao_social_locadora, endereco_locadora) VALUES ("1", "16323", "sei la");
+INSERT INTO locadora_veiculos_locadora (cnpj_locadora, razao_social_locadora, endereco_locadora) VALUES ("2", "23634", "Disney");
+INSERT INTO locadora_veiculos_locadora (cnpj_locadora, razao_social_locadora, endereco_locadora) VALUES ("3", "34365", "Orlando");
+INSERT INTO locadora_veiculos_locadora (cnpj_locadora, razao_social_locadora, endereco_locadora) VALUES ("4", "4554366", "Acre");
+INSERT INTO locadora_veiculos_locadora (cnpj_locadora, razao_social_locadora, endereco_locadora) VALUES ("5", "9434536", "Beverly hills");
 
-INSERT INTO locadora_veiculos_locadora_telefone (telefone) VALUES ("47687394");
-INSERT INTO locadora_veiculos_locadora_telefone (telefone) VALUES ("47635338");
-INSERT INTO locadora_veiculos_locadora_telefone (telefone) VALUES ("47685635");
-INSERT INTO locadora_veiculos_locadora_telefone (telefone) VALUES ("47634636");
-INSERT INTO locadora_veiculos_locadora_telefone (telefone) VALUES ("43657856");
+INSERT INTO locadora_veiculos_telefone (telefone_telefone, cnpj_locadora) VALUES ("1", "1");
+INSERT INTO locadora_veiculos_telefone (telefone_telefone, cnpj_locadora) VALUES ("2", "2");
+INSERT INTO locadora_veiculos_telefone (telefone_telefone, cnpj_locadora) VALUES ("3", "3");
+INSERT INTO locadora_veiculos_telefone (telefone_telefone, cnpj_locadora) VALUES ("4", "4");
+INSERT INTO locadora_veiculos_telefone (telefone_telefone, cnpj_locadora) VALUES ("5", "5");
 
+INSERT INTO locadora_veiculos_tipo_veiculo (descricao_tipo_veiculo, porte_tipo_veiculo, tamanho_tipo_veiculo, numero_passageiros_tipo_veiculo, numero_portas_tipo_veiculo, 
+arcondicionado_tipo_veiculo, multimidia_tipo_veiculo, direcao_hidraulica_tipo_veiculo, cambio_automatico_tipo_veiculo, capacidade_carga_tipo_veiculo) 
+VALUES ("Urbano", "Medío", null, null, null, null, null, null, null, null);
+INSERT INTO locadora_veiculos_tipo_veiculo (descricao_tipo_veiculo, porte_tipo_veiculo, tamanho_tipo_veiculo, numero_passageiros_tipo_veiculo, numero_portas_tipo_veiculo, 
+arcondicionado_tipo_veiculo, multimidia_tipo_veiculo, direcao_hidraulica_tipo_veiculo, cambio_automatico_tipo_veiculo, capacidade_carga_tipo_veiculo) 
+VALUES ("Flex", "Médio", null, null, null, null, null, null, null, null);
+INSERT INTO locadora_veiculos_tipo_veiculo (descricao_tipo_veiculo, porte_tipo_veiculo, tamanho_tipo_veiculo, numero_passageiros_tipo_veiculo, numero_portas_tipo_veiculo, 
+arcondicionado_tipo_veiculo, multimidia_tipo_veiculo, direcao_hidraulica_tipo_veiculo, cambio_automatico_tipo_veiculo, capacidade_carga_tipo_veiculo) 
+VALUES ("Corrida", "Pequeno", null, null, null, null, null, null, null, null);
+INSERT INTO locadora_veiculos_tipo_veiculo (descricao_tipo_veiculo, porte_tipo_veiculo, tamanho_tipo_veiculo, numero_passageiros_tipo_veiculo, numero_portas_tipo_veiculo, 
+arcondicionado_tipo_veiculo, multimidia_tipo_veiculo, direcao_hidraulica_tipo_veiculo, cambio_automatico_tipo_veiculo, capacidade_carga_tipo_veiculo) 
+VALUES ("Futurista", "Médio", null, null, null, null, null, null, null, null);
+INSERT INTO locadora_veiculos_tipo_veiculo (descricao_tipo_veiculo, porte_tipo_veiculo, tamanho_tipo_veiculo, numero_passageiros_tipo_veiculo, numero_portas_tipo_veiculo, 
+arcondicionado_tipo_veiculo, multimidia_tipo_veiculo, direcao_hidraulica_tipo_veiculo, cambio_automatico_tipo_veiculo, capacidade_carga_tipo_veiculo) 
+VALUES ("Viagem", "Grande", null, null, null, null, null, null, null, null);
 
-INSERT INTO locadora_veiculos_tipo_veiculo (descricao, porte, tamanho, numero_passageiros, numero_portas, arcondicionado, multimidia, direcao_hidraulica, cambio_automatico, capacidade_carga) VALUES ("Urbano", "Medío", null, null, null, null, null, null, null, null);
-INSERT INTO locadora_veiculos_tipo_veiculo (descricao, porte, tamanho, numero_passageiros, numero_portas, arcondicionado, multimidia, direcao_hidraulica, cambio_automatico, capacidade_carga) VALUES ("Flex", "Médio", null, null, null, null, null, null, null, null);
-INSERT INTO locadora_veiculos_tipo_veiculo (descricao, porte, tamanho, numero_passageiros, numero_portas, arcondicionado, multimidia, direcao_hidraulica, cambio_automatico, capacidade_carga) VALUES ("Corrida", "Pequeno", null, null, null, null, null, null, null, null);
-INSERT INTO locadora_veiculos_tipo_veiculo (descricao, porte, tamanho, numero_passageiros, numero_portas, arcondicionado, multimidia, direcao_hidraulica, cambio_automatico, capacidade_carga) VALUES ("Futurista", "Médio", null, null, null, null, null, null, null, null);
-INSERT INTO locadora_veiculos_tipo_veiculo (descricao, porte, tamanho, numero_passageiros, numero_portas, arcondicionado, multimidia, direcao_hidraulica, cambio_automatico, capacidade_carga) VALUES ("Viagem", "Grande", null, null, null, null, null, null, null, null);
-//
+INSERT INTO locadora_veiculos_marca (descricao_marca) VALUES ("Mazda");
+INSERT INTO locadora_veiculos_marca (descricao_marca) VALUES ("Honda");
+INSERT INTO locadora_veiculos_marca (descricao_marca) VALUES ("BMW");
+INSERT INTO locadora_veiculos_marca (descricao_marca) VALUES ("Tesla");
+INSERT INTO locadora_veiculos_marca (descricao_marca) VALUES ("Ferrari");
 
-INSERT INTO locadora_veiculos_marca (descricao) VALUES ("Mazda");
-INSERT INTO locadora_veiculos_marca (descricao) VALUES ("Honda");
-INSERT INTO locadora_veiculos_marca (descricao) VALUES ("BMW");
-INSERT INTO locadora_veiculos_marca (descricao) VALUES ("Tesla");
-INSERT INTO locadora_veiculos_marca (descricao) VALUES ("Ferrari");
-//
+INSERT INTO locadora_veiculos_modelo (descricao_modelo, codigo_marca) VALUES ("3 Portas", 1);
+INSERT INTO locadora_veiculos_modelo (descricao_modelo, codigo_marca) VALUES ("Trava Eletrica", 2);
+INSERT INTO locadora_veiculos_modelo (descricao_modelo, codigo_marca) VALUES ("Flex", 3);
+INSERT INTO locadora_veiculos_modelo (descricao_modelo, codigo_marca) VALUES ("Sensor de Ré", 4);
+INSERT INTO locadora_veiculos_modelo (descricao_modelo, codigo_marca) VALUES ("Automatico", 5);
 
-INSERT INTO locadora_veiculos_modelo (codigo_marca, descricao) VALUES (1, "3 Portas");
-INSERT INTO locadora_veiculos_modelo (codigo_marca, descricao) VALUES (2, "Trava Eletrica");
-INSERT INTO locadora_veiculos_modelo (codigo_marca, descricao) VALUES (3, "Flex");
-INSERT INTO locadora_veiculos_modelo (codigo_marca, descricao) VALUES (4, "Sensor de Ré");
-INSERT INTO locadora_veiculos_modelo (codigo_marca, descricao) VALUES (5, "Automatico");
-//
+INSERT INTO locadora_veiculos_veiculo (placa_veiculo, chassi_veiculo, numero_motor_veiculo, status_veiculo, km_atual_veiculo, codigo_modelo, codigo_tipo_veiculo, cnpj_locadora) 
+VALUES ("1", "325345", "32525", "345345", "53636", 1, 1, 1);
+INSERT INTO locadora_veiculos_veiculo (placa_veiculo, chassi_veiculo, numero_motor_veiculo, status_veiculo, km_atual_veiculo, codigo_modelo, codigo_tipo_veiculo, cnpj_locadora) 
+VALUES ("2", "325345", "32525", "345345", "53636", 2, 2, 2);
+INSERT INTO locadora_veiculos_veiculo (placa_veiculo, chassi_veiculo, numero_motor_veiculo, status_veiculo, km_atual_veiculo, codigo_modelo, codigo_tipo_veiculo, cnpj_locadora) 
+VALUES ("3", "325345", "32525", "345345", "53636", 3, 3, 3);
+INSERT INTO locadora_veiculos_veiculo (placa_veiculo, chassi_veiculo, numero_motor_veiculo, status_veiculo, km_atual_veiculo, codigo_modelo, codigo_tipo_veiculo, cnpj_locadora) 
+VALUES ("4", "325345", "32525", "345345", "53636", 4, 4, 4);
+INSERT INTO locadora_veiculos_veiculo (placa_veiculo, chassi_veiculo, numero_motor_veiculo, status_veiculo, km_atual_veiculo, codigo_modelo, codigo_tipo_veiculo, cnpj_locadora) 
+VALUES ("5", "325345", "32525", "345345", "53636", 5, 5, 5);
 
-INSERT INTO locadora_veiculos_veiculo (placa, codigo_modelo, codigo_tipo_veiculo, cnpj_locadora, chassi, numero_motor, status_veiculo, km_atual) VALUES ();
-INSERT INTO locadora_veiculos_veiculo (placa, codigo_modelo, codigo_tipo_veiculo, cnpj_locadora, chassi, numero_motor, status_veiculo, km_atual) VALUES ();
-INSERT INTO locadora_veiculos_veiculo (placa, codigo_modelo, codigo_tipo_veiculo, cnpj_locadora, chassi, numero_motor, status_veiculo, km_atual) VALUES ();
-INSERT INTO locadora_veiculos_veiculo (placa, codigo_modelo, codigo_tipo_veiculo, cnpj_locadora, chassi, numero_motor, status_veiculo, km_atual) VALUES ();
-INSERT INTO locadora_veiculos_veiculo (placa, codigo_modelo, codigo_tipo_veiculo, cnpj_locadora, chassi, numero_motor, status_veiculo, km_atual) VALUES ();
+INSERT INTO locadora_veiculos_cliente (nome_cliente, endereco_cliente) VALUES ("Gustavo", "Rua do Zezinhho 925");
+INSERT INTO locadora_veiculos_cliente (nome_cliente, endereco_cliente) VALUES ("Pedrinho", "Rua do Dirceu 3022");
+INSERT INTO locadora_veiculos_cliente (nome_cliente, endereco_cliente) VALUES ("Andre", "Rua do Campetes 0009");
+INSERT INTO locadora_veiculos_cliente (nome_cliente, endereco_cliente) VALUES ("Robson", "Rua do São Sebastiao 4423");
+INSERT INTO locadora_veiculos_cliente (nome_cliente, endereco_cliente) VALUES ("Moacir", "Rua do Tigre 3635");
 
+INSERT INTO locadora_veiculos_pessoa_fisica (cpf_pessoa_fisica, sexo_pessoa_fisica, data_nascimento_pessoa_fisica, codigo_cliente) VALUES ("12332199989", "Masculino", '1979-09-06', 1);
+INSERT INTO locadora_veiculos_pessoa_fisica (cpf_pessoa_fisica, sexo_pessoa_fisica, data_nascimento_pessoa_fisica, codigo_cliente) VALUES ("12332199989", "Feminino", '2001-01-22', 2);
+INSERT INTO locadora_veiculos_pessoa_fisica (cpf_pessoa_fisica, sexo_pessoa_fisica, data_nascimento_pessoa_fisica, codigo_cliente) VALUES ("12332199989", "Masculino", '1820-04-12', 3);
+INSERT INTO locadora_veiculos_pessoa_fisica (cpf_pessoa_fisica, sexo_pessoa_fisica, data_nascimento_pessoa_fisica, codigo_cliente) VALUES ("12332199989", "Feminino", '1069-08-24', 4);
+INSERT INTO locadora_veiculos_pessoa_fisica (cpf_pessoa_fisica, sexo_pessoa_fisica, data_nascimento_pessoa_fisica, codigo_cliente) VALUES ("12332199989", "Feminino", '1069-08-24', 5);
 
-INSERT INTO locadora_veiculos_cliente (nome,endereco) VALUES ("Gustavo", "Rua do Zezinhho 925");
-INSERT INTO locadora_veiculos_cliente (nome,endereco) VALUES ("Pedrinho", "Rua do Dirceu 3022");
-INSERT INTO locadora_veiculos_cliente (nome,endereco) VALUES ("Andre", "Rua do Campetes 0009");
-INSERT INTO locadora_veiculos_cliente (nome,endereco) VALUES ("Bartolome", "Rua do São Sebastiao 4423");
-INSERT INTO locadora_veiculos_cliente (nome,endereco) VALUES ("Moacir", "Rua do Tigre 3635");
-//
+INSERT INTO locadora_veiculos_pessoa_juridica (cnpj_pessoa_juridica, inscricao_estadual_pessoa_juridica, codigo_cliente) VALUES ("12345678912345", "3881372598269", 1);
+INSERT INTO locadora_veiculos_pessoa_juridica (cnpj_pessoa_juridica, inscricao_estadual_pessoa_juridica, codigo_cliente) VALUES ("98765432198765", "3881083598269", 2);
+INSERT INTO locadora_veiculos_pessoa_juridica (cnpj_pessoa_juridica, inscricao_estadual_pessoa_juridica, codigo_cliente) VALUES ("98765432198700", "3882131598269", 3);
+INSERT INTO locadora_veiculos_pessoa_juridica (cnpj_pessoa_juridica, inscricao_estadual_pessoa_juridica, codigo_cliente) VALUES ("23232323232322", "7532085982269", 4);
+INSERT INTO locadora_veiculos_pessoa_juridica (cnpj_pessoa_juridica, inscricao_estadual_pessoa_juridica, codigo_cliente) VALUES ("69696969696922", "3881089999999", 5);
 
-INSERT INTO locadora_veiculos_pessoa_fisica (cpf, sexo, data_nascimento) VALUES ("12332199989", "Masculino", '1979-09-06');
-INSERT INTO locadora_veiculos_pessoa_fisica (cpf, sexo, data_nascimento) VALUES ("12332199989", "Feminino", '22-01-2001');
-INSERT INTO locadora_veiculos_pessoa_fisica (cpf, sexo, data_nascimento) VALUES ("12332199989", "Masculino", 12-04-1820);
-INSERT INTO locadora_veiculos_pessoa_fisica (cpf, sexo, data_nascimento) VALUES ("12332199989", "Feminino", 24-08-1969);
-INSERT INTO locadora_veiculos_pessoa_fisica (cpf, sexo, data_nascimento) VALUES ("12332199989", "Femninino" 11-09-2001);
+INSERT INTO locadora_veiculos_reserva (data_reserva, data_retirada_veiculo_reserva, status_reserva, codigo_tipo_veiculo, codigo_cliente) VALUES ('2010-12-18', '2010-12-18', "1", 1, 1);
+INSERT INTO locadora_veiculos_reserva (data_reserva, data_retirada_veiculo_reserva, status_reserva, codigo_tipo_veiculo, codigo_cliente) VALUES ('2004-04-25', '2004-04-25', "2", 2, 2);
+INSERT INTO locadora_veiculos_reserva (data_reserva, data_retirada_veiculo_reserva, status_reserva, codigo_tipo_veiculo, codigo_cliente) VALUES ('1965-06-16', '1965-06-16', "3", 3, 3);
+INSERT INTO locadora_veiculos_reserva (data_reserva, data_retirada_veiculo_reserva, status_reserva, codigo_tipo_veiculo, codigo_cliente) VALUES ('1994-08-03', '1994-08-03', "4", 4, 4);
+INSERT INTO locadora_veiculos_reserva (data_reserva, data_retirada_veiculo_reserva, status_reserva, codigo_tipo_veiculo, codigo_cliente) VALUES ('2016-09-16', '2016-09-16', "5", 5, 5);
 
-
-INSERT INTO locadora_veiculos_pessoa_juridica (cnpj, inscricao_estadual) VALUES ("12345678912345", "3881372598269");
-INSERT INTO locadora_veiculos_pessoa_juridica (cnpj, inscricao_estadual) VALUES ("98765432198765", "3881083598269");
-INSERT INTO locadora_veiculos_pessoa_juridica (cnpj, inscricao_estadual) VALUES ("98765432198700", "3882131598269");
-INSERT INTO locadora_veiculos_pessoa_juridica (cnpj, inscricao_estadual) VALUES ("23232323232322", "7532085982269");
-INSERT INTO locadora_veiculos_pessoa_juridica (cnpj, inscricao_estadual) VALUES ("69696969696922", "3881089999999");
-
-
-INSERT INTO locadora_veiculos_reserva (codigo_tipo_veiculo, codigo_cliente, data_reserva, data_retirada_veiculo) VALUES (1, 1, 18/12/2010, 18/12/2010);
-INSERT INTO locadora_veiculos_reserva (codigo_tipo_veiculo, codigo_cliente, data_reserva, data_retirada_veiculo) VALUES (2, 2, 25/12/2004, 25/12/2004);
-INSERT INTO locadora_veiculos_reserva (codigo_tipo_veiculo, codigo_cliente, data_reserva, data_retirada_veiculo) VALUES (3, 3, 29/03/1965, 29/03/1965);
-INSERT INTO locadora_veiculos_reserva (codigo_tipo_veiculo, codigo_cliente, data_reserva, data_retirada_veiculo) VALUES (4, 4, 05/07/1994, 05/07/1994);
-INSERT INTO locadora_veiculos_reserva (codigo_tipo_veiculo, codigo_cliente, data_reserva, data_retirada_veiculo) VALUES (5, 5, 16/01/2016, 16/01/2016);
-
-
-INSERT INTO locadora_veiculos_aluguel (placa, cnpj_locadora_origem, cnpj_locadora_destino, codigo_cliente, data_ ,data_prevista_entrega, data_entrega,nome_motorista,cnh_motorista, data_validade, km_rodada, valor) VALUES ();
-INSERT INTO locadora_veiculos_aluguel (placa, cnpj_locadora_origem, cnpj_locadora_destino, codigo_cliente, data_ ,data_prevista_entrega, data_entrega,nome_motorista,cnh_motorista, data_validade, km_rodada, valor) VALUES ();
-INSERT INTO locadora_veiculos_aluguel (placa, cnpj_locadora_origem, cnpj_locadora_destino, codigo_cliente, data_ ,data_prevista_entrega, data_entrega,nome_motorista,cnh_motorista, data_validade, km_rodada, valor) VALUES ();
-INSERT INTO locadora_veiculos_aluguel (placa, cnpj_locadora_origem, cnpj_locadora_destino, codigo_cliente, data_ ,data_prevista_entrega, data_entrega,nome_motorista,cnh_motorista, data_validade, km_rodada, valor) VALUES ();
-INSERT INTO locadora_veiculos_aluguel (placa, cnpj_locadora_origem, cnpj_locadora_destino, codigo_cliente, data_ ,data_prevista_entrega, data_entrega,nome_motorista,cnh_motorista, data_validade, km_rodada, valor) VALUES ();
+INSERT INTO locadora_veiculos_aluguel (data_aluguel, data_prevista_entrega_aluguel, data_entrega_aluguel, nome_motorista_aluguel, cnh_motorista_aluguel, data_validade_cnh_aluguel, km_rodada_aluguel, 
+valor_aluguel, placa_veiculo, cnpj_locadora_origem, cnpj_locadora_destino, codigo_cliente) VALUES ('1678-12-30', '1678-12-30', '1678-12-30', "Bartolomel", "1", '1678-12-30', 1, 1, "1", "1", "1", 1);
+INSERT INTO locadora_veiculos_aluguel (data_aluguel, data_prevista_entrega_aluguel, data_entrega_aluguel, nome_motorista_aluguel, cnh_motorista_aluguel, data_validade_cnh_aluguel, km_rodada_aluguel, 
+valor_aluguel, placa_veiculo, cnpj_locadora_origem, cnpj_locadora_destino, codigo_cliente) VALUES ('1944-06-14', '1944-06-14', '1944-06-14', "Cleiton", "2", '1944-06-14', 2, 2, "2", "2", "2", 2);
+INSERT INTO locadora_veiculos_aluguel (data_aluguel, data_prevista_entrega_aluguel, data_entrega_aluguel, nome_motorista_aluguel, cnh_motorista_aluguel, data_validade_cnh_aluguel, km_rodada_aluguel, 
+valor_aluguel, placa_veiculo, cnpj_locadora_origem, cnpj_locadora_destino, codigo_cliente) VALUES ('2015-05-12', '2015-05-12', '2015-05-12', "Jonas", "3", '2015-05-12', 3, 3, "3", "3", "3", 3);
+INSERT INTO locadora_veiculos_aluguel (data_aluguel, data_prevista_entrega_aluguel, data_entrega_aluguel, nome_motorista_aluguel, cnh_motorista_aluguel, data_validade_cnh_aluguel, km_rodada_aluguel, 
+valor_aluguel, placa_veiculo, cnpj_locadora_origem, cnpj_locadora_destino, codigo_cliente) VALUES ('2019-03-05', '2019-03-05', '2019-03-05', "Jubilel", "4", '2019-03-05', 4, 4, "4", "4", "4", 4);
+INSERT INTO locadora_veiculos_aluguel (data_aluguel, data_prevista_entrega_aluguel, data_entrega_aluguel, nome_motorista_aluguel, cnh_motorista_aluguel, data_validade_cnh_aluguel, km_rodada_aluguel, 
+valor_aluguel, placa_veiculo, cnpj_locadora_origem, cnpj_locadora_destino, codigo_cliente) VALUES ('2018-01-03', '2018-01-03', '2018-01-03', "Choriscreuda", "5", '2018-01-03', 5, 5, "5", "5", "5", 5);
