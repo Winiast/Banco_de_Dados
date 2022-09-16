407e888e5c3df47cc0f8e3761d4ae12e1abebe71
Gustavo Ferreira <82901156+Winiast@users.noreply.github.com>
2022-09-16 20:49:35 -0300

    Criado tela de cadastro de contato

--- Atividade Final/bancoDeDados/src/main/java/ifpr/pagua/com/banco/controllers/CadastroContato.java
+++ Atividade Final/bancoDeDados/src/main/java/ifpr/pagua/com/banco/controllers/CadastroContato.java
@@ -1,5 +1,28 @@
 package ifpr.pagua.com.banco.controllers;
 
+import ifpr.pagua.com.banco.App;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
 public class CadastroContato {
+
+    private TextField nome;
+
+    private TextField email;
+
+    private TextField telefone;
+
+    private Button cadastrar;
+
+    private Button voltarTela;
+
+    public void cadastrarContato(ActionEvent Event) {
+        System.out.println("Cadastrar Contato");
+    }
+
+    public void voltarTelaInicial(ActionEvent Event) {
+        App.popScreen();
+    }
 
 }
